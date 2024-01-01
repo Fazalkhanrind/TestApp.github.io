71bba5fa50654111547ec0aa0b908bce30fd6b7f
Fazal khan <fazalkhanrind786@gmail.com>
2024-01-02 00:20:31 +0500

    this is my first test web hosting
    this is my first test web hosting

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        } 
+        public IActionResult Index()
+        {
+            return View();
+        } 
+    }
+}
